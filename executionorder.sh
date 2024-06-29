9b0dbc0a973090f06fefb8d25fefdf2e57311ef6
nnfuad <nnfuad01@gmail.com>
2024-06-30 00:24:37 +0600

    added fourth file

---
+++ executionorder.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+directory=/etsdfgsdfi
+
+if [ -d $directory ]
+then
+	echo $?
+	echo "The directory $directory exists"
+	echo $?
+else
+	echo $?
+	echo "The directory $directory doesn't exist"
+	echo $?
+fi
+	echo $?
+echo "The exit code for this script run is $?"
